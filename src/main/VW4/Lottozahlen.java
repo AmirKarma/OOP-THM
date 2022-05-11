f50305b209278829267c141e09b114a539cb065a
Amir Ebrahimi <99660768+amirkarma@users.noreply.github.com>
2022-05-11 23:08:06 +0200

    finished Lottozahlen project v1.0
    Signed-off-by: AmirKarma <amir.ebrahimi0k@gmail.com>

--- src/main/VW4/Lottozahlen.java
+++ src/main/VW4/Lottozahlen.java
@@ -5,43 +5,36 @@ public class Lottozahlen {
     static int[] doppelteZahlen = new int[50];
 
     public static void main(String[] args) {
-        // initialisiere mit Zufallszahlen zwischen 1 und 49
-        initArray();
-        // Suche nach doppelten Zahlen
-        // TODO: Besser -> zähle doppelte Zahlen
-        sucheDoppelte();
-        // Ausgabe wie oft die Zahlen zwischen 1 und 49 bei den 100
-        // Zufallszahlen vorkommt
-        // TODO: Methodengebundene Ausgaben an den Anfang der Methode schieben
-        System.out.println("Auswertung");
-        ausgabeArray(doppelteZahlen);
-        // Suche nach dem erstmaligen Vorkommen einer doppelten Zahl
-        // Ausgabe der Position
-        //System.out.println("Erste doppelte Zahl bei: " + indexErsteDoppelte(zahlen));
-
-        // Ausgabe eines Arrays: Index und Wert
-        ausgabeArray(zahlen);
+// initialisiere mit Zufallszahlen zwischen 1 und 49
+        initArray ();
+// Ausgabe eines Arrays: Index und Wert
+        ausgabeArray ( zahlen );
 // Ausgabe von Maximal-, Minimal- und Durchschnitwert
-        System.out.println("Maximalwert: " + maxWert());
-        System.out.println("Minimalwert: " + minWert());
-        System.out.println("Mittelwert: " + midWert());
+        System.out.println ( "Maximalwert: " + maxWert () );
+        System.out.println ( "Minimalwert: " + minWert () );
+        System.out.println ( "Mittelwert: " + midWert () );
+// Suche nach doppelten Zahlen
+        sucheDoppelte ();
+// Ausgabe wie oft die Zahlen zwischen 1 und 49 bei den 100
+// Zufallszahlen vorkommt
+        System.out.println ( "Auswertung" );
+        ausgabeArray ( doppelteZahlen );
+// Suche nach dem erstmaligen Vorkommen einer doppelten Zahl
+// Ausgabe der Position
+        System.out.println ( "Erste doppelte Zahl bei: " + indexErsteDoppelte ( zahlen ) );
+
     }
 
     public static void initArray() {
-        Random zufall = new Random();
+        Random zufall = new Random ();
         for (int i = 0; i < zahlen.length; i++) {
-            zahlen[i] = zufall.nextInt(1, 49);
-        }
-    }
-    public static void sucheDoppelte() {
-        //TODO: for(i...zahlen.length) -> doppelteZahlen[i]++;
-        for (int i = 0; i < zahlen.length; i++) {
+            zahlen[i] = zufall.nextInt ( 1, 50 );
         }
     }
 
     public static void ausgabeArray(int[] zahlen) {
         for (int i = 0; i < zahlen.length; i++) {
-            System.out.println(i + 1 + ":\t" + zahlen[i]);
+            System.out.println ( i + 1 + ":\t" + zahlen[i] );
         }
     }
 
@@ -68,4 +61,24 @@ public class Lottozahlen {
         }
         return (double) sum / zahlen.length;
     }
-}
+
+    public static void sucheDoppelte() {
+        for (int k : zahlen) {
+            for (int j = 1; j <= doppelteZahlen.length; j++) {
+                if (k == j) doppelteZahlen[j - 1] += 1;
+            }
+        }
+        }
+
+        public static int indexErsteDoppelte(int[] zahlen){
+        int place = Integer.MAX_VALUE;
+        for(int i=0; i<zahlen.length - 1; i++){
+            for(int j= i + 1; j<zahlen[i]; j++){
+                if(zahlen[i] == zahlen[j]) {
+                    if (place > j) place = j;
+                }
+            }
+        }
+        return place + 1;
+        }
+    }
